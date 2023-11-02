15e123ef2f49a184dee0506a8c17e777e3b6223f
chinagxwei <chinagxwei@gmail.com>
2023-11-02 13:19:04 +0800

    init

--- database/migrations/2023_09_14_012248_create_wallet_withdrawals_table.php
+++ database/migrations/2023_09_14_012248_create_wallet_withdrawals_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->string('third_party_payment_sn',64)->nullable()->comment('第三方支付ID');
             $table->string('third_party_merchant_id',18)->nullable()->comment('第三方支付商户号');
             $table->bigInteger('amount')->unsigned()->nullable()->comment('金额（单位：分）');
+            $table->tinyInteger('status')->default(0)->nullable()->comment('状态 -1审核失败 0待审核 1审核成功');
+            $table->string('remark',128)->nullable()->comment('备注');
             $table->string('sign',64)->nullable()->comment('签名');
             $table->integer('created_at')->unsigned()->nullable();
             $table->integer('updated_at')->unsigned()->nullable();
